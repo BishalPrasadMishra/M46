2fca49242bcb3d4b652f1a8e6cdf7362b7a01f69
BishalPrasadMishra <bishalprasadm@gmail.com>
2026-02-27 11:50:27 +0530

    commit

---
+++ M46/src/test/java/demo/Dummy.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Dummy {
+	public static void main(String[] args) {
+		System.out.println("Bishal");
+		System.out.println("Bishal");
+		System.out.println("Bishal");
+	}
+}
